60b04004f8957e9d526226e4531d3f0ba0d8a17a
sweet-poteto <sagarpardhi.dev@outlook.com>
2024-01-19 12:53:28 +0530

    add github authentication

---
+++ components/providers/convex-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+import React from "react";
+
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL as string
+);
+
+export const ConvexClientProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+    >
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://model-pheasant-54.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
